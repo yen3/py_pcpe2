9692f558d9c0abf323bbd8058e6cb27eab9c186d
yen3 <yen3rc@gmail.com>
2017-04-29 19:16:12 +0800

    Initial the project with a python package.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Python scripts for PCPE2
 
-* Under construction.
+* The project is under construction.
+
+## Requirement
+
+* Python 3.6
+* virtualenv
+
+## Install
+
+```
+git clone  --recursive https://github.com/yen3/py_pcpe2 py_pcpe2
+cd py_pcpe2
+virualenv -p python3 .env
+source .env/bin/activate
+python setup.py install
+```
+
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'py_pcpe2',
+    version = '2.2.0',
+    author = 'yen3',
+    author_email = 'yen3rc@gmail.com',
+    packages = find_packages(exclude=['examples', 'tests', 'results']),
+    include_package_data = True,
+    entry_points = {
+        'console_scripts': ['max_comsubseq.py=py_pcpe2.main:main'],
+    },
+    url = 'https://github.com/yen3/py_pcpe2',
+)
