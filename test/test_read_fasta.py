4238ac59fd702dacdc952d75264943c0f7c599f2
yen3 <yen3rc@gmail.com>
2017-05-03 21:17:21 +0800

    Refine coding style.

--- test/test_read_fasta.py
+++ test/test_read_fasta.py
@@ -5,27 +5,34 @@ import os.path
 import test.env
 
 from pypcpe2 import read_fasta
+from pypcpe2.env import env
 
 
 class TestReadFASTA(unittest.TestCase):
     def setUp(self):
         self.test_data_folder = test.env.test_data_folder
-        self.test_data_output_folder = test.env.test_output_folder
+        self.test_output_folder = test.env.test_output_folder
 
         self.seqfile1 = read_fasta.SeqFile(
             os.path.join(self.test_data_folder, "test1.txt"),
             seq_path=os.path.join(self.test_data_folder, "test1_seq.txt"),
             id_path=os.path.join(self.test_data_folder, "test1_id.txt"),
-            id_info_path=os.path.join(self.test_data_folder, "test1_id_info.txt"))
+            id_info_path=os.path.join(self.test_data_folder,
+                                      "test1_id_info.txt"))
 
         self.seqfile2 = read_fasta.SeqFile(
             os.path.join(self.test_data_folder, "test2.txt"),
             seq_path=os.path.join(self.test_data_folder, "test2_seq.txt"),
             id_path=os.path.join(self.test_data_folder, "test2_id.txt"),
-            id_info_path=os.path.join(self.test_data_folder, "test2_id_info.txt"))
+            id_info_path=os.path.join(self.test_data_folder,
+                                      "test2_id_info.txt"))
+
+        self.saved_temp_path = env().temp_path
+        env().temp_path = self.test_output_folder
+        self.temp_path = env().temp_path
 
     def tearDown(self):
-        pass
+        env().temp_path = self.saved_temp_path
 
     def compare_sorted_file_content(self, x_path, y_path):
         with open(x_path) as fx, open(y_path) as fy:
@@ -38,7 +45,7 @@ class TestReadFASTA(unittest.TestCase):
 
     def test_env(self):
         self.assertTrue(os.path.isdir(self.test_data_folder))
-        self.assertTrue(os.path.isdir(self.test_data_output_folder))
+        self.assertTrue(os.path.isdir(self.test_output_folder))
 
     def test_retrieve_fasta_id(self):
         id_line1 = ">gi|9999999999|gb|AOS87590|pypcpe2 test sequence1"
@@ -49,25 +56,35 @@ class TestReadFASTA(unittest.TestCase):
         self.assertEqual(read_fasta.retrieve_fasta_id(id_line2), '1034563939')
 
     def test_read_fasta_file_seq1(self):
-        fs = [fasta for fasta in read_fasta.read_fasta_file(self.seqfile1.fasta_path)]
+        fs = [fasta for fasta in
+              read_fasta.read_fasta_file(self.seqfile1.fasta_path)]
 
-        ans = [('>gi|9999999999|gb|AOS87590|pypcpe2 test sequence1', 'ABCDEFG'),
-               ('>gi|9999999998|gb|AOS87590|pypcpe2 test sequence2', 'ABCDEFGH'),
-               ('>gi|9999999997|gb|AOS87590|pypcpe2 test sequence3', 'ABCDEFGH'),
-               ('>gi|9999999996|gb|AOS87590|pypcpe2 test sequence4', 'ABCDEFGH'),
-               ('>gi|9999999995|gb|AOS87590|pypcpe2 test sequence5', 'ABCDEFGHI'),
-               ('>gi|9999999994|gb|AOS87590|pypcpe2 test sequence6', 'ABCDEFGHI')]
+        ans = [('>gi|9999999999|gb|AOS87590|pypcpe2 test sequence1',
+                'ABCDEFG'),
+               ('>gi|9999999998|gb|AOS87590|pypcpe2 test sequence2',
+                'ABCDEFGH'),
+               ('>gi|9999999997|gb|AOS87590|pypcpe2 test sequence3',
+                'ABCDEFGH'),
+               ('>gi|9999999996|gb|AOS87590|pypcpe2 test sequence4',
+                'ABCDEFGH'),
+               ('>gi|9999999995|gb|AOS87590|pypcpe2 test sequence5',
+                'ABCDEFGHI'),
+               ('>gi|9999999994|gb|AOS87590|pypcpe2 test sequence6',
+                'ABCDEFGHI')]
 
         self.assertEqual(ans, fs)
 
     def test_read_fasta_file_seq2(self):
-        fs = [fasta for fasta in read_fasta.read_fasta_file(self.seqfile2.fasta_path)]
+        fs = [fasta for fasta in
+              read_fasta.read_fasta_file(self.seqfile2.fasta_path)]
 
         ans = [('>gi|9999999993|gb|AOS87590|pypcpe2 test sequence7', 'BCDEFG'),
                ('>gi|9999999992|gb|AOS87590|pypcpe2 test sequence8', 'BCDEFG'),
                ('>gi|9999999991|gb|AOS87590|pypcpe2 test sequence9', 'CDEFGHI'),
-               ('>gi|9999999990|gb|AOS87590|pypcpe2 test sequence10', 'CDEFGHI'),
-               ('>gi|9999999989|gb|AOS87590|pypcpe2 test sequence11', 'CDEFGHI')]
+               ('>gi|9999999990|gb|AOS87590|pypcpe2 test sequence10',
+                'CDEFGHI'),
+               ('>gi|9999999989|gb|AOS87590|pypcpe2 test sequence11',
+                'CDEFGHI')]
 
         self.assertEqual(ans, fs)
 
@@ -96,8 +113,8 @@ class TestReadFASTA(unittest.TestCase):
         ans_seq_path = self.seqfile1.seq_path
         ans_id_path = self.seqfile1.id_path
 
-        seq_path = os.path.join(self.test_data_output_folder, 'test1_seq.txt')
-        id_path = os.path.join(self.test_data_output_folder, 'test1_id.txt')
+        seq_path = os.path.join(self.test_output_folder, 'test1_seq.txt')
+        id_path = os.path.join(self.test_output_folder, 'test1_id.txt')
 
         read_fasta.create_seq_id_file(fasta_path, seq_path, id_path)
 
@@ -110,8 +127,8 @@ class TestReadFASTA(unittest.TestCase):
         ans_seq_path = self.seqfile2.seq_path
         ans_id_path = self.seqfile2.id_path
 
-        seq_path = os.path.join(self.test_data_output_folder, 'test2_seq.txt')
-        id_path = os.path.join(self.test_data_output_folder, 'test2_id.txt')
+        seq_path = os.path.join(self.test_output_folder, 'test2_seq.txt')
+        id_path = os.path.join(self.test_output_folder, 'test2_id.txt')
 
         read_fasta.create_seq_id_file(fasta_path, seq_path, id_path)
 
