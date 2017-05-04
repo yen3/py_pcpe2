a89e9d2987cc99801cf3b52bfa5f300f0fdf8c4c
yen3 <yen3rc@gmail.com>
2017-05-04 17:27:51 +0800

    Add pybind11 package

---
+++ core_src/pcpe2_core.cc
@@ -0,0 +1,99 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+int subtract(int i, int j) {
+    return i - j;
+}
+
+void print_string(std::string& s) {
+  std::cout << s << std::endl;
+}
+
+std::string return_the_same_string(std::string& s) {
+  std::string s1(s);
+  return s1;
+}
+
+std::vector<std::string> add_string_suffix(std::vector<std::string>& ss) {
+  std::vector<std::string> rs;
+
+  for (const auto& s : ss) {
+    rs.emplace_back(s + "_cpp11");
+  }
+
+  return rs;
+}
+
+
+namespace py = pybind11;
+
+PYBIND11_PLUGIN(python_example) {
+    py::module m("python_example", R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+
+        .. currentmodule:: python_example
+
+        .. autosummary::
+           :toctree: _generate
+
+           add
+           subtract
+    )pbdoc");
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", &subtract, R"pbdoc(
+        Subtract two numbers
+
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+    m.def("print_str", &print_string, R"pbdoc(
+      Print a string to stdout.
+    )pbdoc");
+
+    m.def("return_str", &return_the_same_string, R"pbdoc(
+      Return the same string.
+    )pbdoc");
+
+    m.def("add_str_list_suffix", &add_string_suffix, R"pbdoc(
+      Return the list of string with suffix.
+    )pbdoc");
+
+    m.def("add_str_list_suffix2",
+        [](const std::vector<std::string>& ss) -> std::vector<std::string> {
+          std::vector<std::string> rs;
+
+          for (const auto& s : ss) {
+            rs.emplace_back(s + "_cpp11");
+          }
+
+          return rs;
+        }, R"pbdoc(
+      Return the list of string with suffix.
+    )pbdoc");
+
+
+
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = py::str(VERSION_INFO);
+#else
+    m.attr("__version__") = py::str("dev");
+#endif
+
+    return m.ptr();
+}
--- setup.py
+++ setup.py
@@ -1,20 +1,112 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+import subprocess
+import shlex
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
+ext_modules = [
+    Extension(
+        'python_example',
+        ['core_src/pcpe2_core.cc'],
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True)
+        ],
+        language='c++'
+    ),
+]
+
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14] compiler flag.
+
+    The c++14 is prefered over c++11 (when it is available).
+    """
+    if has_flag(compiler, '-std=c++14'):
+        return '-std=c++14'
+    elif has_flag(compiler, '-std=c++11'):
+        return '-std=c++11'
+    else:
+        raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                           'is needed!')
+
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+    }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+        elif ct == 'msvc':
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
+
+
 
 def read_requirements():
-    with open('./requirements.txt', 'r') as f:
-        return f.read().split()
+    with open('./requirements.txt', 'r') as fin:
+        return fin.read().split()
 
     return []
+
+subprocess.call(shlex.split("pip install -r requirements.txt"))
 
 setup(
     name='pypcpe2',
     version='2.2.0',
     author='yen3',
     author_email='yen3rc@gmail.com',
+    install_requires=read_requirements(),
     packages=find_packages(exclude=['examples', 'tests', 'results',
                                     'core', 'core_src', 'temp']),
-    install_requires=read_requirements(),
-    include_package_data=True,
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': BuildExt},
     test_suite='nose.collector',
     tests_require=['nose'],
     entry_points={
