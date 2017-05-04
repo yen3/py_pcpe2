88df928b6c7e56a75e4ea5aac36e2202e6dde987
yen3 <yen3rc@gmail.com>
2017-05-04 17:15:11 +0800

    Refine coding style

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
+def read_requirements():
+    with open('./requirements.txt', 'r') as f:
+        return f.read().split()
+
+    return []
+
 setup(
-    name = 'pypcpe2',
-    version = '2.2.0',
-    author = 'yen3',
-    author_email = 'yen3rc@gmail.com',
-    packages = find_packages(exclude=['examples', 'tests', 'results']),
-    include_package_data = True,
+    name='pypcpe2',
+    version='2.2.0',
+    author='yen3',
+    author_email='yen3rc@gmail.com',
+    packages=find_packages(exclude=['examples', 'tests', 'results',
+                                    'core', 'core_src', 'temp']),
+    install_requires=read_requirements(),
+    include_package_data=True,
     test_suite='nose.collector',
     tests_require=['nose'],
-    entry_points = {
+    entry_points={
         'console_scripts': ['max_comsubseq.py=pypcpe2.main:main'],
     },
-    url = 'https://github.com/yen3/pypcpe2',
+    url='https://github.com/yen3/pypcpe2',
 )
