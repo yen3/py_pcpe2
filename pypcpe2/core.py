de9bc72da81d787232fddd58ed564a495dc0829b
yen3 <yen3rc@gmail.com>
2017-05-05 17:17:16 +0800

    Add core module and test cases

--- pypcpe2/core.py
+++ pypcpe2/core.py
@@ -1,37 +1,39 @@
-import shutil
-
 from pypcpe2 import utility
 
 import pcpe2_core
 
 def compare_small_seqs(x_seq_path, y_seq_path):
+    """
+    Wrapper for pcpe2_core.compare_small_seqs.
+
+    Users should not call the orignal function directly.
+    """
     return pcpe2_core.compare_small_seqs(x_seq_path, y_seq_path)
 
 
 def sort_comsubseq_files(input_paths):
+    """
+    Wrapper for pcpe2_core.sort_comsubseq_files.
+
+    Users should not call the orignal function directly.
+    """
     return pcpe2_core.sort_comsubseq_files(input_paths)
 
 
-def search_max_comsubseq(input_paths):
-    return pcpe2_core.max_sorted_comsubsq_files
+def max_sorted_comsubsq_files(input_paths):
+    """
+    Wrapper for pcpe2_core.max_sorted_comsubsq_files.
 
-
-def combine_comsubseq_files(input_paths, output_path):
-	with open(output_path, 'wb') as fout:
-		for path in input_paths:
-			with open(path, 'rb') as fin:
-				shutil.copyfileobj(fin, fout)
+    Users should not call the orignal function directly.
+    """
+    return pcpe2_core.max_sorted_comsubsq_files(input_paths)
 
 
 def compare_seqs(x_seq_path, y_seq_path):
-
     seq_paths = compare_small_seqs(x_seq_path, y_seq_path)
-
     sorted_paths = sort_comsubseq_files(seq_paths)
-
-    max_seq_paths = search_max_comsubseq(max_seq_paths)
-
+    max_seq_paths = max_sorted_comsubsq_files(sorted_paths)
     output_path = utility.make_temp_path("cs_result.bin")
-    combine_comsubseq_files(max_combseqs_paths, output_path)
+    utility.merge_file(max_seq_paths, output_path)
 
     return output_path
--- test/test_core.py
+++ test/test_core.py
@@ -21,15 +21,16 @@ class TestCore(unittest.TestCase):
         env().temp_path = self.test_output_folder
         self.temp_path = env().temp_path
 
-        pcpe2_core.env_set_temp_path(env().temp_path)
+        self.saved_output_min_len = env().output_min_len
+        env().output_min_len = 6
 
     def tearDown(self):
         env().temp_path = self.saved_temp_path
+        env().output_min_len = self.saved_output_min_len
 
     def test_compare_small_seqs(self):
         seq1_path = os.path.join(self.test_data_folder, "test1_seq.txt")
         seq2_path = os.path.join(self.test_data_folder, "test2_seq.txt")
-
 
         cs_paths = core.compare_small_seqs(seq1_path, seq2_path)
 
@@ -54,6 +55,38 @@ class TestCore(unittest.TestCase):
 
         self.assertEqual(len(sorted_paths), 1)
         seqs = comsubseq.read_comsubseq_file(sorted_paths[0])
+
+        self.assertEqual(seqs, ans)
+
+    def test_max_sorted_comsubsq_files(self):
+        sorted_paths = [os.path.join(self.test_data_folder,
+                                     "sorted_compare_hash_0")]
+
+        max_paths = core.max_sorted_comsubsq_files(sorted_paths)
+        self.assertEqual(len(max_paths), 1)
+        seqs = comsubseq.read_comsubseq_file(max_paths[0])
+
+        ans = [comsubseq.ComSubseq(0, 0, 1, 0, 6),
+               comsubseq.ComSubseq(1, 0, 1, 0, 6),
+               comsubseq.ComSubseq(1, 1, 2, 0, 6),
+               comsubseq.ComSubseq(2, 0, 1, 0, 6),
+               comsubseq.ComSubseq(2, 1, 2, 0, 7)]
+
+        self.assertEqual(seqs, ans)
+
+    def test_compare_seqs(self):
+        seq1_path = os.path.join(self.test_data_folder, "test1_seq.txt")
+        seq2_path = os.path.join(self.test_data_folder, "test2_seq.txt")
+        result_path = os.path.join(self.test_output_folder, "cs_result.bin")
+
+        core.compare_seqs(seq1_path, seq2_path)
+        seqs = comsubseq.read_comsubseq_file(result_path)
+
+        ans = [comsubseq.ComSubseq(0, 0, 1, 0, 6),
+               comsubseq.ComSubseq(1, 0, 1, 0, 6),
+               comsubseq.ComSubseq(1, 1, 2, 0, 6),
+               comsubseq.ComSubseq(2, 0, 1, 0, 6),
+               comsubseq.ComSubseq(2, 1, 2, 0, 7)]
 
         self.assertEqual(seqs, ans)
 
