13d748850dbd142ff7b6e2a7dbcb5ebf918b2276
yen3 <yen3rc@gmail.com>
2017-05-05 15:40:35 +0800

    Add core module: A lightful wrapper for pcpe2_core.

--- pypcpe2/core.py
+++ pypcpe2/core.py
@@ -1,21 +1,29 @@
+import shutil
+
+from pypcpe2 import utility
+
+import pcpe2_core
+
 def compare_small_seqs(x_seq_path, y_seq_path):
-    return []
+    return pcpe2_core.compare_small_seqs(x_seq_path, y_seq_path)
 
 
 def sort_comsubseq_files(input_paths):
-    return []
+    return  pcpe2_core.sort_comsubseq_files(input_paths)
 
 
 def search_max_comsubseq(input_paths):
-    return []
+    return pcpe2_core.max_sorted_comsubsq_files
 
 
 def combine_comsubseq_files(input_paths, output_path):
-    return None
+	with open(output_path, 'wb') as fout:
+		for path in input_paths:
+			with open(path, 'rb') as fin:
+				shutil.copyfileobj(fin, fout)
 
 
 def compare_seqs(x_seq_path, y_seq_path):
-    output_path = "temp"
 
     seq_paths = compare_small_seqs(x_seq_path, y_seq_path)
 
@@ -23,8 +31,7 @@ def compare_seqs(x_seq_path, y_seq_path):
 
     max_seq_paths = search_max_comsubseq(max_seq_paths)
 
+    output_path = utility.make_temp_path("cs_result.bin")
     combine_comsubseq_files(max_combseqs_paths, output_path)
 
     return output_path
-
-
