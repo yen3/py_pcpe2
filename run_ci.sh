a79a897cac05494054d144fa08280f845c348934
yen3 <yen3rc@gmail.com>
2017-05-07 19:16:37 +0800

    Fix typo.

--- run_ci.sh
+++ run_ci.sh
@@ -1,9 +1,13 @@
 #/usr/bin/env bash
 
+
 export DEBIAN_FRONTEND=noninteractive
 
+rm -rf /var/lib/dpkg/lock
 apt-get update
-apt-get install -y python3 python3-dev gcc g++ python-virtualenv virtualenv -p python3 .env
+apt-get install -y python3 python3-dev gcc g++ python-virtualenv
+
+virtualenv -p python3 .env
 source .env/bin/activate
 
 pip install -r requirements.txt
