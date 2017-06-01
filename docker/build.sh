98eee726bbb1e324870b754a8d75df2e0811473f
yen3 <yen3rc@gmail.com>
2017-06-01 11:34:13 +0800

    Use python docker to build the package

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-docker build -t python3 . -f Dockerfile
+
+cp Dockerfile Dockerfile_3_4
+sed -i 's/3.4/3.4/g' Dockerfile_3_4
+
+cp Dockerfile Dockerfile_3_5
+sed -i 's/3.4/3.5/g' Dockerfile_3_5
+
+cp Dockerfile Dockerfile_3_6
+sed -i 's/3.4/3.6/g' Dockerfile_3_6
+
+
+docker build -t python34 . -f Dockerfile_3_4
+docker build -t python35 . -f Dockerfile_3_5
+docker build -t python36 . -f Dockerfile_3_6
+
+rm -rf Dockerfile_3_4
+rm -rf Dockerfile_3_5
+rm -rf Dockerfile_3_6
--- docker/test.sh
+++ docker/test.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
 
-docker run --rm --name test_pypcpe2 python3 ./test_docker.sh
+function run_test
+{
+  docker run --rm --name test_pypcpe2_$1 python$1 ./test_docker.sh
+}
+
+function print_result
+{
+  if [[ $2 -eq 0 ]]; then
+      echo "Python$1: success"
+  else
+      echo "Python$1: fail"
+  fi
+}
+
+run_test 34
+TEST_34=$?
+
+run_test 35
+TEST_35=$?
+
+run_test 36
+TEST_36=$?
+
+echo "\nTest PyPCPE2 in:"
+print_result "3.4" ${TEST_34}
+print_result "3.5" ${TEST_35}
+print_result "3.6" ${TEST_36}
