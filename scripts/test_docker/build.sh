f5ace3ddd5bf6093d054592e93ab15d6066a62a4
yen3 <yen3rc@gmail.com>
2017-06-01 11:54:19 +0800

    Abstract these actions with a function let the file is more readable

--- scripts/test_docker/build.sh
+++ scripts/test_docker/build.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-cp Dockerfile Dockerfile_3_4
-sed -i 's/3.4/3.4/g' Dockerfile_3_4
+function build_image
+{
+  cp Dockerfile Dockerfile_$1
+  sed -i "s/3.4/$2/g" Dockerfile_$1
+  docker build -t python$1 . -f Dockerfile_$1
+  rm -rf Dockerfile_$1
+}
 
-cp Dockerfile Dockerfile_3_5
-sed -i 's/3.4/3.5/g' Dockerfile_3_5
-
-cp Dockerfile Dockerfile_3_6
-sed -i 's/3.4/3.6/g' Dockerfile_3_6
-
-
-docker build -t python34 . -f Dockerfile_3_4
-docker build -t python35 . -f Dockerfile_3_5
-docker build -t python36 . -f Dockerfile_3_6
-
-rm -rf Dockerfile_3_4
-rm -rf Dockerfile_3_5
-rm -rf Dockerfile_3_6
+build_image "34" "3.4"
+build_image "35" "3.5"
+build_image "36" "3.6"
